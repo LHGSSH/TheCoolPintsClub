75f156c5b7542593a565cdb26735d8f408dc9f0a
Liam Hilton-Green <liamhg1@yahoo.com>
2020-10-10 16:29:07 -0500

    Set up JWT.

--- user-service/models/user.js
+++ user-service/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 //defines the User Schema
 let userSchema = mongoose.Schema({
@@ -9,5 +10,17 @@ let userSchema = mongoose.Schema({
     address: String,
     apiKey: String
 });
+
+userSchema.methods.generateJwt = function () {
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        fullName: this.fullName,
+        apiKey: this.apiKey,
+        exp: parseInt(expiry.getTime() / 1000, 10),
+    }, 'thisIsSecret');
+};
 
 mongoose.model('User', userSchema);
