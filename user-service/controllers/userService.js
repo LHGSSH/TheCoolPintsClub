7540dec7d4743ddb32599aeeb808d23bc46e3a25
Liam Hilton-Green <liamhg1@yahoo.com>
2020-10-31 11:31:20 -0500

    Commented out api tokens.

--- user-service/controllers/userService.js
+++ user-service/controllers/userService.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
-const token = process.env.TOKEN || "recipeT0k3n";
+// const token = process.env.TOKEN || "recipeT0k3n";
 
 module.exports = {
     /**
@@ -22,26 +22,26 @@ module.exports = {
         });
     },
 
-    /**
-     * Middleware function to verify API token in usersController, called in a separate part
-     * @param {Number} id 
-     * @param {JSON} newUserDetails 
-     */
-    verifyToken: (req, res, next) => {
-        let token = req.query.apiToken;
-        if (token) {
-            User.findOne({ apiToken: token })
-                .then(user => {
-                    if (user) next();
-                    else next(new Error("Invalid API token."));
-                })
-                .catch(error => {
-                    next(new Error(error.message));
-                });
-        } else {
-            next(new Error("Invalid API token."));
-        }
-    },
+    // /**
+    //  * Middleware function to verify API token in usersController, called in a separate part
+    //  * @param {Number} id 
+    //  * @param {JSON} newUserDetails 
+    //  */
+    // verifyToken: (req, res, next) => {
+    //     let token = req.query.apiToken;
+    //     if (token) {
+    //         User.findOne({ apiToken: token })
+    //             .then(user => {
+    //                 if (user) next();
+    //                 else next(new Error("Invalid API token."));
+    //             })
+    //             .catch(error => {
+    //                 next(new Error(error.message));
+    //             });
+    //     } else {
+    //         next(new Error("Invalid API token."));
+    //     }
+    // },
 
     /**
      * Edits a user in the database.
