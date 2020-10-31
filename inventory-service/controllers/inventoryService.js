1546a10d686dacc2ff84a1b6de49f367feec275f
MattMinish <mjm79@students.uwf.edu>
2020-10-31 12:31:14 -0500

    inventory service is done

--- inventory-service/controllers/inventoryService.js
+++ inventory-service/controllers/inventoryService.js
@@ -6,7 +6,13 @@ module.exports = {
      * 
      */
     search: async function (req, res) {
-        return await Inventory.findOne({ flavor: 'Apple Cinnamon' })
+        await Inventory.findOne({flavor: req.body}, function (err, result) {
+            if (err) {
+                return res.status(404).json(err);
+            } else {
+                res.status(200).json({ result });
+            }
+        })(req, res);
     },
 
     testsearch: async function (req, res) {
