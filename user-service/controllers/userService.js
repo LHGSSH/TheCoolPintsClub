dabc8af3cdbfaa40aba7af16db53acf7ba78f63c
Michael Sohns <ms270@students.uwf.edu>
2020-10-31 09:34:22 -0500

    Working on API Token

--- user-service/controllers/userService.js
+++ user-service/controllers/userService.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
 
+// Check for the API Key, called in a separate part
+verifyToken: (req, res, next) => { if (req.query.apiToken === token) next(); else next(new Error("Invalid API token.")); }
+
+
 module.exports = {
     /**
      * Adds a user to the database. Also generates an API key and a JWT for the user.
@@ -27,6 +31,9 @@ module.exports = {
      * @param {JSON} newUserDetails 
      */
     editUser: function (id, newUserDetails) {
+        //Mongoose to find API token--Compare it
+        var token = req.query.token;
+        // jwt.verify(token,)
         return User.findByIdAndUpdate(id, newUserDetails, { new: true })
             .catch(err => {
                 console.error(err);
@@ -40,6 +47,9 @@ module.exports = {
      * @param {any} res
      */
     login: function (req, res) {
+        // Check API Key
+        // var token = req.query.token;
+        
         passport.authenticate('local', (err, user, info) => {
             let token;
             if (err) {
