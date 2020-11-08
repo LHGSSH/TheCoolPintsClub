7c00683df920219eaa5ad48bd17ce33cdf229f95
ConnerH50 <crh50@students.uwf.edu>
2020-11-08 12:19:14 -0600

    made initial searchpage component

---
+++ ice-cream-service/src/app/checkoutpage/checkoutpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkoutpage',
+  templateUrl: './checkoutpage.component.html',
+  styleUrls: ['./checkoutpage.component.css']
+})
+export class CheckoutpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
